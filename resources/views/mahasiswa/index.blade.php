0fb6f932fbc648750fdae44c06c96ffdd87cfdf5
ansoriid <ahmadansori9090@gmail.com>
2024-08-06 13:51:20 +0700

    blade layout update

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('contents')
 
-@include('layouts.sidebar-kaprodi')
+@include('layouts.sidebar-mahasiswa')
 
 <div class="w-full overflow-x-hidden border-t flex flex-col">
             <main class="w-full flex-grow p-6">
